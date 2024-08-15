5b58a64d5718bd92fc6e0b7e0b9ab9712d6999b4
Ernesto Argenzio <ernemogilevich@gmail.com>
2024-08-15 09:57:39 -0300

    Adding fixes to comply with activity

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,34 @@
+let alertSuccess = document.getElementById("alert-success");
+let alertDanger = document.getElementById("alert-danger");
+
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+    alertSuccess.style = "display:block; opacity: 1";
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    alertDanger.style = "display:block; opacity: 1";
 }
+
+function closeAlert() {
+    alertSuccess.style = "display:none";
+    alertDanger.style = "display:none";
+}
+
+let regBtn = document.getElementById("regBtn");
+let inputs = document.getElementsByTagName("input");
+
+//Get inputs
+let inputNombre = document.getElementById("nombre");
+let inputApellido = document.getElementById("apellido");
+let inputEmail = document.getElementById("email");
+let inputPwd = document.getElementById("password1");
+let inputPwd2 = document.getElementById("password2");
+let checkbox = document.getElementById("terminos");
+
+regBtn.addEventListener("click", function() {
+    if((inputNombre.value.length > 0 && inputApellido.value.length > 0 && inputEmail.value.length > 0 && inputPwd.value.length > 5 && inputPwd2.value.length > 5) && (inputPwd.value === inputPwd2.value) && terminos.checked) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+});
