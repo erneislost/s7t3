b3997e3d59de9d25569a9f59329a96a4b2284138
Ernesto Argenzio <ernemogilevich@gmail.com>
2024-08-15 10:33:59 -0300

    Commenting and removing unused variables

--- js/validacion.js
+++ js/validacion.js
@@ -1,21 +1,25 @@
+//Obtener divs de alertas
 let alertSuccess = document.getElementById("alert-success");
 let alertDanger = document.getElementById("alert-danger");
 
+//Mostrar alerta correcta
 function showAlertSuccess() {
     alertSuccess.style = "display:block; opacity: 1";
 }
 
+//Mostrar alerta error
 function showAlertError() {
     alertDanger.style = "display:block; opacity: 1";
 }
 
+//Ocultar alerta/s
 function closeAlert() {
     alertSuccess.style = "display:none";
     alertDanger.style = "display:none";
 }
 
+//Obtener botón
 let regBtn = document.getElementById("regBtn");
-let inputs = document.getElementsByTagName("input");
 
 //Get inputs
 let inputNombre = document.getElementById("nombre");
@@ -25,10 +29,13 @@ let inputPwd = document.getElementById("password1");
 let inputPwd2 = document.getElementById("password2");
 let checkbox = document.getElementById("terminos");
 
+//Manejador de evento click en el botón
 regBtn.addEventListener("click", function() {
     if((inputNombre.value.length > 0 && inputApellido.value.length > 0 && inputEmail.value.length > 0 && inputPwd.value.length > 5 && inputPwd2.value.length > 5) && (inputPwd.value === inputPwd2.value) && terminos.checked) {
+        //Mostrar alerta correcta cuando se cumplen los requisitos
         showAlertSuccess();
     } else {
+        //Mostrar error cuando este no es el caso
         showAlertError();
     }
 });
